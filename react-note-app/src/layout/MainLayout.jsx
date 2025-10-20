3afaf38a825af5b9d52fc289ecc357019ee2db51
idrice junior <dongmojunior18@gmail.com>
2025-10-20 15:09:43 -0500

    last

--- react-note-app/src/layout/MainLayout.jsx
+++ react-note-app/src/layout/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -18,66 +18,63 @@ const MainLayout = ({ searchText, handleSearchText, isAuthenticated, setIsAuthen
         <Outlet />
       </div>
 
-   <footer className="layout-footer bg-light py-3 mt-4">
-  <div className="container text-center" style={{ marginLeft: "5%" }}>
-    <div className="mb-2">
-      &copy; {new Date().getFullYear()} My NoteBook App. All rights reserved.
-    </div>
+  
+            <footer className="p-5 bg-light text-dark text-center
+                        position-relative custom-shadow">
+                        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css" integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossOrigin="anonymous"></link>
+                        <div className="container text-center" style={{ marginLeft: "5%" }}>
+                <div className="mb-2">
+                  &copy; {new Date().getFullYear()} My NoteBook App. All rights reserved.
+                </div>
 
-    <div className="mb-2">
-      <a
-        href="https://idricejunior.onrender.com"
-        target="_blank"
-        rel="noreferrer"
-        className="text-decoration-none"
-      >
-        Get in touch with me!
-      </a>
-    </div>
+                <div className="mb-2">
+                  <a
+                    href="https://idricejunior.onrender.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-decoration-none"
+                  >
+                    Get in touch with me!
+                  </a>
+                </div>
 
-    <div className="mb-2">
-      Built with React & Django |
-      <a
-        href="https://github.com/Junior-idrice"
-        target="_blank"
-        rel="noreferrer"
-        className="ms-1 text-decoration-none"
-      >
-        GitHub
-      </a>
-    </div>
+                <div className="mb-2">
+                  Built with React & Django |
+                  <a
+                    href="https://github.com/Junior-idrice"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="ms-1 text-decoration-none"
+                  >
+                    GitHub
+                  </a>
+                </div>
 
-    <div>
-      Follow me:{" "}
-      <a
-        href="https://www.instagram.com/idricejr/?igsh=MWt2ZjUybHZuamczcg%3D%3D#"
-        target="_blank"
-        rel="noreferrer"
-        className="text-decoration-none"
-      >
-        Instagram
-      </a>{" "}
-      |{" "}
-      <a
-        href="https://www.linkedin.com/in/junior-idrice/"
-        target="_blank"
-        rel="noreferrer"
-        className="text-decoration-none"
-      >
-        LinkedIn
-      </a>
-    </div>
-  </div>
-</footer>
- <footer class="p-5 bg-light text-dark text-center
-            position-relative custom-shadow">
-            <div class="container">
-                <p class="lead">Copyright &copy; 2025 Frontend Developer</p>
-                <a href="https://www.linkedin.com/in/junior-idrice/"><i class="bi bi-linkedin text-dark mx-1"></i></a>
-                <a href="https://www.instagram.com/idricejr?igsh=MWt2ZjUybHZuamczcg=="><i class="bi bi-instagram text-dark mx-1"></i></a>
-               
-            </div>
-
+                <div>
+                  Follow me:{" "}
+                  <a
+                    href="https://www.instagram.com/idricejr/?igsh=MWt2ZjUybHZuamczcg%3D%3D#"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-decoration-none"
+                  >
+                    Instagram
+                  </a>{" "}
+                  |{" "}
+                  <a
+                    href="https://www.linkedin.com/in/junior-idrice/"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="text-decoration-none"
+                  >
+                    LinkedIn
+                  </a>
+                </div>
+                  <Link href="https://www.linkedin.com/in/junior-idrice/"><i className="bi bi-linkedin text-dark mx-1"></i></Link>
+                            <Link href="https://www.instagram.com/idricejr?igsh=MWt2ZjUybHZuamczcg=="><i className="bi bi-instagram text-dark mx-1"></i></Link>
+                            
+              </div>
+                        
             </footer>
 
 
